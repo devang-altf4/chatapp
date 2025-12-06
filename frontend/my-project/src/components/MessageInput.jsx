1018a6c0a315d3c69083cf5fec2f19e38fbe53b8
Devang gupta <maybedevang29@gmail.com>
2025-12-06 17:31:49 +0530

    ui

--- frontend/my-project/src/components/MessageInput.jsx
+++ frontend/my-project/src/components/MessageInput.jsx
@@ -190,11 +190,6 @@ const MessageInput = () => {
           </div>
         </div>
       )}
-      <button type="button" className="btn-more" title="More options">
-        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15M17 8L12 3M12 3L7 8M12 3V15" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        </svg>
-      </button>
       <button type="submit" className="btn-send" disabled={!message.trim()}>
         <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M22 2L11 13M22 2L15 22L11 13M22 2L2 9L11 13" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
